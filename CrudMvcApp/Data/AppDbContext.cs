41dd5e92d4d3be96262139fcdd0667178d1bd121
Agus <agusgus612@gmail.com>
2026-01-16 21:18:19 +0700

    Add Class AppDbContext

---
+++ CrudMvcApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudMvcApp.Models;
+
+namespace CrudMvcApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options): base(options) 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
