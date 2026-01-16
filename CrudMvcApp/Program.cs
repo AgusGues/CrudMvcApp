9537963ab2f8b61968f246c45a90f776da6a308e
Agus <agusgus612@gmail.com>
2026-01-16 21:24:16 +0700

    setting koneksi database

--- CrudMvcApp/Program.cs
+++ CrudMvcApp/Program.cs
@@ -1,7 +1,14 @@
+using CrudMvcApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//pendaftaran koneksi database
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
