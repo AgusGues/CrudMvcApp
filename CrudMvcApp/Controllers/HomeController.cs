d5045757bee65a4c56aa127a37807eb2f4198dd8
Agus <agusgus612@gmail.com>
2026-01-18 01:33:56 +0700

    upload file

---
+++ CrudMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Products");
+        }
+    }
+}
